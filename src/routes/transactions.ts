7e592be79e8e71c4f414be4c6fac52919cf1ec84
sain419 <lildestro7@gmail.com>
2026-06-13 20:17:42 +0100

    security layer 4: fraud detection rules

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 import { createTransaction, getTransactions } from '../controllers/transactionController'
 import { protect } from '../middleware/auth'
+import { fraudCheck } from '../middleware/fraudDetection'
 
 const router = Router()
 
-router.post('/', protect, createTransaction)
+router.post('/', protect, fraudCheck, createTransaction)
 router.get('/', protect, getTransactions)
 
 export default router
